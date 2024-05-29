6a6b47c3f0af85b4d94d00fb37734b62953c2347
Tanya <t2.luskatova@gmail.com>
2024-05-29 16:39:12 +0200

    adding and removing movies

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,6 +8,7 @@ const appDataExample = {
 				"Kate Winslet"
 			],
 			poster: 'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQXUpVhfnjzY01pU1p4ta9hEhQ3gGsCooCyJ3M890P9UwzPG_yJW2EzvdnYta43RX8u',
+			link: '#',
 			id: 1
 		},
 		{
@@ -18,6 +19,7 @@ const appDataExample = {
 				"Wang Yibo"
 			],
 			poster: 'https://upload.wikimedia.org/wikipedia/en/3/31/Theuntamed.jpg',
+			link: '#',
 			id: 2
 		},
 		{
@@ -36,6 +38,7 @@ const appDataExample = {
 				"Johnny Depp"
 			],
 			poster: 'https://upload.wikimedia.org/wikipedia/en/3/3c/Fantastic_Beasts_-_The_Crimes_of_Grindelwald_Poster.png',
+			link: '#',
 			id: 3
 		},
 		{
@@ -49,6 +52,7 @@ const appDataExample = {
 				"Shelley Hennig"
 			],
 			poster: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/6967c7f3-1f18-439f-b20e-1fa0334656c8/1920x',
+			link: '#',
 			id: 4
 		},
 		{
@@ -71,22 +75,68 @@ const appDataExample = {
 				"Orlando Bloom"
 			],
 			poster: 'https://upload.wikimedia.org/wikipedia/en/e/e7/The_Hobbit_-_The_Battle_of_the_Five_Armies.png',
+			link: '#',
 			id: 5
 		}
 	]
 };
 
+const appDataDefolt = {
+	films: []
+};
+
 class NewFilm {
 	appData;
+	filmDelId;
+	delPopupElement;
 
 	constructor() {
-		this.appData = appDataExample;
+		this.appData = this.getAppData();
+		console.log(this.appData);
+		this.addFormListener();
 		this.renderFilmList();
+		this.delPopupElement = this.getDelPopupElement();
 		this.sortSelectBlock = this.getSortSelectBlock();
 		this.addSortSelectBlockClickHandlers();
-		this.getFormData();
+		this.addDelPopupClickHandlers();
+		this.addDelButtonClickEventListener();
 	}
 
+
+	addFormListener() {
+		const formElement = document.getElementById('film_form');
+		formElement.addEventListener('submit', (e) => {
+			e.preventDefault();
+			const formData = this.getFormData(formElement);
+			formData.id = this.getNewRecordId();
+			this.appData.films.unshift(formData);
+			this.safeAppData();
+			this.renderFilmList();
+		});
+	}
+
+	getFormData(formElement) {
+		const title = formElement.querySelector("[name=title]");
+		const year = formElement.querySelector("[name=year]");
+		const cast = formElement.querySelector("[name=cast]");
+		const poster = formElement.querySelector("[name=poster]");
+
+		const data = {
+			title: title.value,
+			year: year.value,
+			cast: cast.value.split(',').map((person) => person.trim()),
+			poster: poster.value
+		};
+		return data;
+	}
+
+	getNewRecordId() {
+		const idArray = this.appData.films.map((film) => {
+			return film.id;
+		});
+		const maxId = Math.max(...idArray);
+		return maxId + 1;
+	}
 
 	renderFilmList() {
 		let htmlString = '';
@@ -121,31 +171,19 @@ class NewFilm {
 
 		let wrapMovies = document.getElementById('movies');
 		wrapMovies.innerHTML = htmlString;
+
+		console.log(wrapMovies);
 	}
 
-	getFormData() {
-		const formElement = document.getElementById('film_form'); // get the form element
-		formElement.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const formData = new FormData(formElement); // create a FormData object, pass the form element to it
-		// now we can retrieve the data
-		const title = formData.get("title");
-		const year = formData.get("year");
-		const cast = formData.get("cast");
-		const poster = formData.get("poster");
-		const link = formData.get("link");
+	getDelPopupElement() {
+		return document.querySelector('.modal');
+	}
+	showDelPopup() {
+		this.delPopupElement.style.display = 'flex';
+	}
 
-		const data = {
-			title: title,
-			year: year,
-			cast: cast,
-			poster: poster,
-			link: link
-		};
-		console.log(data);
-		return data;
-	});
-
+	hideDelPopup() {
+		this.delPopupElement.style.display = '';
 	}
 	getSortSelectBlock() {
 		return document.querySelector('.movie_sort_options');
@@ -174,6 +212,64 @@ class NewFilm {
 				this.showSortSelectBlock();
 			}
 		});
+	}
+
+	addDelPopupClickHandlers() {
+		const delPopupCrossBtn = document.querySelector('.del_popup_cross_btn');
+		const delPopupAbortBtn = document.querySelector('.del_popup_abort_btn');
+		const delPopupConfirmBtn = document.querySelector('.del_popup_confirm_btn');
+		delPopupCrossBtn.addEventListener("click", () => {
+			this.hideDelPopup();
+			this.filmDelId = null;
+		});
+		delPopupAbortBtn.addEventListener("click", () => {
+			this.hideDelPopup();
+			this.filmDelId = null;
+		});
+		delPopupConfirmBtn.addEventListener("click", () => {
+			this.delFilmRecord();
+			this.hideDelPopup();
+			this.filmDelId = null;
+		});
+		this.delPopupElement.addEventListener("click", (event) => {
+			if (event.target.closest(".del_popup") === null) {
+				this.hideDelPopup();
+				this.filmDelId = null;
+			}
+		});
+	}
+
+	addDelButtonClickEventListener() {
+		let filmsContainer = document.querySelector('.films_container');
+		filmsContainer.addEventListener("click", (event ) => {
+			const clickedDelButton = event.target;
+			const filmLi = clickedDelButton.closest(".film");
+			this.filmDelId = +filmLi.id;
+			this.showDelPopup();
+		});
+	}
+	delFilmRecord() {
+		this.appData.films = this.appData.films.filter((filmItem) => {
+			return filmItem.id !== this.filmDelId;
+		});
+		this.safeAppData();
+		this.renderFilmList();
+		this.addDelButtonClickEventListener();
+	}
+
+	getAppData() {
+		const localAppData = JSON.parse(localStorage.getItem('keyData'));
+
+		if (localAppData === null) {
+			// return appDataDefolt;
+			return appDataExample;
+		} else {
+			return localAppData;
+		}
+	}
+
+	safeAppData() {
+		localStorage.setItem('keyData', JSON.stringify(this.appData));
 	}
 
 }
